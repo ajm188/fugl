e5ae0be3b6fc2f5396c308033ca8faf08dd6b37e
Andrew Mason <andrew@fixedpoint.xyz>
2016-04-03 16:48:28 -0400

    use reverse instead of explicit URL. this removes the trailing slash

--- fugl/main/views/root.py
+++ fugl/main/views/root.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.shortcuts import redirect
-from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def root_controller(request):
     if request.method == 'GET':
-        return redirect('/home/')
+        return redirect(reverse('home'))
     else:
         return HttpResponse(status=500)
