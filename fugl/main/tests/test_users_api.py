809218a98fb7a3b423a6dbc47262cff3eda7dee9
Andrew Mason <andrew@fixedpoint.xyz>
2016-04-17 19:08:17 -0400

    move path inside class

--- fugl/main/tests/test_users_api.py
+++ fugl/main/tests/test_users_api.py
@@ -9,10 +9,9 @@ from main.models import User
 from .base import FuglTestCase
 
 
-user_path = '/users/'
-
-
 class UserViewSetTestCase(FuglTestCase):
+
+    user_path = '/users/'
 
     def setUp(self):
         super().setUpTheme()
@@ -24,7 +23,7 @@ class UserViewSetTestCase(FuglTestCase):
     def test_create_user(self):
         post_data = {'username': 'test_user', 'password': 'test_pass',
                      'email': 'example@example.com'}
-        response = self.client.post(user_path, post_data)
+        response = self.client.post(self.user_path, post_data)
         self.assertEqual(response.status_code, 201)
         users = User.objects.filter(username='test_user')
         self.assertEqual(len(users), 1)
@@ -37,7 +36,7 @@ class UserViewSetTestCase(FuglTestCase):
 
         post_data = {'username': 'test_user', 'password': 'test_pass',
                      'email': 'example@example.com'}
-        response = self.client.post(user_path, post_data)
+        response = self.client.post(self.user_path, post_data)
         self.assertEqual(response.status_code, 400)
         users = User.objects.filter(username='test_user')
         self.assertEqual(len(users), 1)
@@ -47,7 +46,7 @@ class UserViewSetTestCase(FuglTestCase):
     def test_post_invalid_email_fails(self):
         post_data = {'username': 'test_user', 'password': 'test_pass',
                      'email': 'I am not really an email address!'}
-        response = self.client.post(user_path, post_data)
+        response = self.client.post(self.user_path, post_data)
         self.assertEqual(response.status_code, 400)
         self.assertIn(b'Enter a valid email address', response.content)
         users = User.objects.filter(username='test_user')
@@ -56,7 +55,7 @@ class UserViewSetTestCase(FuglTestCase):
     def test_post_invalid_username_with_spaces_fails(self):
         post_data = {'username': 'test user', 'password': 'test_pass',
                      'email': 'example@example.com'}
-        response = self.client.post(user_path, post_data)
+        response = self.client.post(self.user_path, post_data)
         self.assertEqual(response.status_code, 400)
         self.assertIn(b'Enter a valid username', response.content)
         users = User.objects.filter(username='test_user')
@@ -65,7 +64,7 @@ class UserViewSetTestCase(FuglTestCase):
     def test_post_invalid_username_invalid_character_fails(self):
         post_data = {'username': 'test/user', 'password': 'test_pass',
                      'email': 'example@example.com'}
-        response = self.client.post(user_path, post_data)
+        response = self.client.post(self.user_path, post_data)
         self.assertEqual(response.status_code, 400)
         self.assertIn(b'Enter a valid username', response.content)
         users = User.objects.filter(username='test_user')
@@ -74,7 +73,7 @@ class UserViewSetTestCase(FuglTestCase):
     def test_post_invalid_username_too_long_fails(self):
         post_data = {'username': '0123456789012345678901234567890',
                      'password': 'test_pass', 'email': 'example@example.com'}
-        response = self.client.post(user_path, post_data)
+        response = self.client.post(self.user_path, post_data)
         self.assertEqual(response.status_code, 400)
         self.assertIn(b'Ensure this field has no more than', response.content)
         users = User.objects.filter(username='test_user')
