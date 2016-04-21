b0bc13affa0b3f5b1eeb340a068e452c2ced8541
Andrew Mason <andrew@fixedpoint.xyz>
2016-04-21 19:57:31 -0400

    add listing for pages

--- fugl/main/api/pages.py
+++ fugl/main/api/pages.py
@@ -14,14 +14,29 @@ from main.util import UserAccess
 class PageViewSet(viewsets.GenericViewSet):
 
     queryset = Page.objects.all()
+    project_queryset = Project.objects.all()
     serializer_class = PageSerializer
     permission_classes = (IsAuthenticated,)
+
+    def list(self, request):
+        if 'project' not in request.query_params:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        proj_id = request.query_params['project']
+        project = get_object_or_404(self.project_queryset, pk=proj_id)
+
+        if UserAccess(request.user).can_view(project):
+            pages = self.queryset.filter(project=project)
+            serializer = self.serializer_class(pages, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
     def create(self, request):
         if 'project' not in request.data:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        project = get_object_or_404(Project, pk=request.data['project'])
+        project = get_object_or_404(self.project_queryset,
+            pk=request.data['project'])
         access = UserAccess(request.user)
         if access.can_edit(project):
             serializer = self.serializer_class(data=request.data)
