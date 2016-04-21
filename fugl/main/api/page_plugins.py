1f8f107b3f0c26e1c48ec0db98aef398fa70a0d7
Andrew Mason <andrew@fixedpoint.xyz>
2016-04-21 17:53:29 -0400

    page plugins create action

--- fugl/main/api/page_plugins.py
+++ fugl/main/api/page_plugins.py
@@ -30,3 +30,21 @@ class PagePluginViewSet(viewsets.GenericViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def create(self, request):
+        if 'project' not in request.data:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        proj_id = request.data['project']
+        project = get_object_or_404(self.project_queryset, pk=proj_id)
+
+        if UserAccess(request.user).can_edit(project):
+            serializer = self.serializer_class(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,
+                    status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
--- fugl/main/tests/api/test_page_plugins.py
+++ fugl/main/tests/api/test_page_plugins.py
@@ -83,3 +83,122 @@ class ListPagePluginTestCase(FuglViewTestCase):
     def test_list_bad_data(self):
         resp = self.client.get(self.url, {})
         self.assertEqual(resp.status_code, 400)
+
+
+class CreatePagePluginTestCase(FuglViewTestCase):
+
+    url = '/page_plugins/'
+
+    def setUp(self):
+        super().setUp()
+
+        self.project = self.create_project('project', 'descr',
+            owner=self.admin_user)
+        self.plug1 = self.create_page_plugin('plug1', project=self.project)
+
+        self.other_user = self.create_user('other')
+        self.other_project = self.create_project('other', 'descr',
+            owner=self.other_user)
+
+        self.login(user=self.admin_user)
+
+    def tearDown(self):
+        self.other_project.delete()
+        self.other_user.delete()
+        self.plug1.delete()
+        self.project.delete()
+
+        super().tearDown()
+
+    def test_create_success(self):
+        count = self.project.pageplugin_set.count()
+        data = {
+            'project': self.project.id,
+            'title': 'plug2',
+            'head_markup': 'something',
+            'body_markup': 'something else',
+        }
+
+        resp = self.client.post(self.url, data=data)
+        self.assertEqual(resp.status_code, 201)
+
+        data = resp.data
+        self.assertEqual(data.get('title', None), 'plug2')
+        self.assertEqual(self.project.pageplugin_set.count(), count + 1)
+        new_plugs = self.project.pageplugin_set.filter(title='plug2')
+        [p.delete() for p in new_plugs]
+
+    def test_bad_data(self):
+        count = self.project.pageplugin_set.count()
+        data = {
+            'project': self.project.id,
+            'head_markup': 'something',
+        }
+
+        resp = self.client.post(self.url, data=data)
+        self.assertEqual(resp.status_code, 400)
+        self.assertIn('title', resp.data)
+        self.assertIn('body_markup', resp.data)
+        self.assertNotIn('head_markup', resp.data)
+        self.assertEqual(self.project.pageplugin_set.count(), count)
+
+        data = {}
+        resp = self.client.post(self.url, data=data)
+        self.assertEqual(resp.status_code, 400)
+
+    def test_create_duplicate(self):
+        count = self.project.pageplugin_set.count()
+        data = {
+            'project': self.project.id,
+            'title': 'plug1',
+            'head_markup': 'something',
+            'body_markup': 'something',
+        }
+
+        resp = self.client.post(self.url, data=data)
+        self.assertEqual(resp.status_code, 400)
+        self.assertIn('non_field_errors', resp.data)
+        self.assertEqual(self.project.pageplugin_set.count(), count)
+
+    def test_create_edit_access(self):
+        access = self.create_access(self.admin_user, self.other_project,
+            can_edit=True)
+        count = self.other_project.pageplugin_set.count()
+        data = {
+            'project': self.other_project.id,
+            'title': 'plug2',
+            'head_markup': 'something',
+            'body_markup': 'something',
+        }
+
+        resp = self.client.post(self.url, data=data)
+        self.assertEqual(resp.status_code, 201)
+        self.assertEqual(
+            self.other_project.pageplugin_set.count(),
+            count + 1,
+        )
+
+        access.delete()
+        self.other_project.pageplugin_set.all()[count].delete()
+
+    def test_create_view_access(self):
+        data = {
+            'project': self.other_project.id,
+            'title': 'plug2',
+            'head_markup': 'something',
+            'body_markup': 'something',
+        }
+
+        resp = self.client.post(self.url, data=data)
+        self.assertEqual(resp.status_code, 404)
+
+    def test_non_existent_project(self):
+        data = {
+            'project': -1,
+            'title': 'plug2',
+            'head_markup': 'something',
+            'body_markup': 'something',
+        }
+
+        resp = self.client.post(self.url, data=data)
+        self.assertEqual(resp.status_code, 404)
